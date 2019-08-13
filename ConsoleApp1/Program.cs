9ccd99fd75316f90f4a85b5756026a2a30348f84
Ezkillz <47401914+Ezkillz@users.noreply.github.com>
2019-08-13 18:47:42 -0500

    Add files via upload
    Added grade book, finished PoCos , and list item

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+    }
+    class DriversLincense
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string genderType { get; set; }
+        public string licenseNumber { get; set; }
+
+        public DriversLincense(string First, string Last, string Gendder, string License)
+        {
+            firstName = First;
+            lastName = Last;
+            genderType = Gendder;
+            licenseNumber = License;
+        }
+        public String GetFullName()
+        {
+            string fullName = firstName + " " + lastName;
+            return fullName;
+        }
+    }
+
+}
